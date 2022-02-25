f2221d39565437931e73163718db975e14f2defb
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-02-25 08:20:33 -0600

    fix es6 syntax

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,63 +1,63 @@
 module.exports = {
-	"$schema": "http://json.schemastore.org/prettierrc",
-	"arrowParens": "always",
-	"bracketSameLine": false,
-	"bracketSpacing": true,
-	"embeddedLanguageFormatting": "auto",
-	"endOfLine": "lf",
-	"htmlWhitespaceSensitivity": "css",
-	"insertPragma": false,
-	"jsxSingleQuote": true,
-	"printWidth": 125,
-	"quoteProps": "as-needed",
-	"semi": true,
-	"singleQuote": true,
-	"tabWidth": 4,
-	"trailingComma": "es5",
-	"useTabs": true,
-	"vueIndentScriptAndStyle": false,
-	"overrides": [
+	$schema: 'http://json.schemastore.org/prettierrc',
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxSingleQuote: true,
+	printWidth: 125,
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	vueIndentScriptAndStyle: false,
+	overrides: [
 		{
-			"files": [
-				"nuget.config",
-				"*.props",
-				"*.filters",
-				"*.vcxproj",
-				"*.targets",
-				"*.xaml",
-				"packages.config",
-				"*.appxmanifest"
+			files: [
+				'nuget.config',
+				'*.props',
+				'*.filters',
+				'*.vcxproj',
+				'*.targets',
+				'*.xaml',
+				'packages.config',
+				'*.appxmanifest',
 			],
-			"options": {
-				"parser": "xml"
-			}
+			options: {
+				parser: 'xml',
+			},
 		},
 		{
-			"files": ["*.yml", "*.yaml"],
-			"options": {
-				"singleQuote": false,
-				"useTabs": false,
-				"tabWidth": 2
-			}
+			files: ['*.yml', '*.yaml'],
+			options: {
+				singleQuote: false,
+				useTabs: false,
+				tabWidth: 2,
+			},
 		},
 		{
-			"files": ["*.md", "*.html"],
-			"options": {
-				"singleQuote": false,
-				"printWidth": 100
-			}
+			files: ['*.md', '*.html'],
+			options: {
+				singleQuote: false,
+				printWidth: 100,
+			},
 		},
 		{
-			"files": ["*.svg"],
-			"options": {
-				"parser": "xml"
-			}
+			files: ['*.svg'],
+			options: {
+				parser: 'xml',
+			},
 		},
 		{
-			"files": ["*.ejs"],
-			"options": {
-				"parser": "glimmer"
-			}
-		}
-	]
+			files: ['*.ejs'],
+			options: {
+				parser: 'glimmer',
+			},
+		},
+	],
 };
